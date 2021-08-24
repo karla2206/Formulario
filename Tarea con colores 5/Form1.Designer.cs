a029fac7c10f715f9e6811624f4b261ef84143ce
karla2206 <88003228+karla2206@users.noreply.github.com>
2021-08-23 22:23:57 -0600

    Add files via upload

---
+++ Tarea con colores 5/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Tarea_con_colores_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aCercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aCercaDeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button1.Location = new System.Drawing.Point(61, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ACEPTAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button2.Location = new System.Drawing.Point(369, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 86);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ACEPTAR/CANCELAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button3.Location = new System.Drawing.Point(703, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(233, 86);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SI/NO";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button4.Location = new System.Drawing.Point(61, 290);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(233, 86);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "EXCLAMACIÒN ";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button5.Location = new System.Drawing.Point(369, 290);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(233, 86);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "INTERROGACIÒN";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button6.Location = new System.Drawing.Point(703, 290);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(233, 86);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "ERROR";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aCercaDeToolStripMenuItem,
+            this.aCercaDeToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1031, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aCercaDeToolStripMenuItem
+            // 
+            this.aCercaDeToolStripMenuItem.Name = "aCercaDeToolStripMenuItem";
+            this.aCercaDeToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.aCercaDeToolStripMenuItem.Text = "Ayuda";
+            // 
+            // aCercaDeToolStripMenuItem1
+            // 
+            this.aCercaDeToolStripMenuItem1.Name = "aCercaDeToolStripMenuItem1";
+            this.aCercaDeToolStripMenuItem1.Size = new System.Drawing.Size(74, 20);
+            this.aCercaDeToolStripMenuItem1.Text = "A cerca de";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(1031, 497);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Constantia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Practica de formularios";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aCercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aCercaDeToolStripMenuItem1;
+    }
+}
+
---
+++ Tarea con colores 5/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_con_colores_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ha aceptado los terminos de este formulario", "Mensaje con botòn Aceptar");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ha aceptado o cancelado los terminos de este formulario", "Mensaje con botòn Aceptar/Cancelar");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ha aceptado o rechazado los terminos de este formulario", "Mensaje con botòn Si/No");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ha presionado el botòn de exclamaciòn", "Mensaje con botòn Exclamaciòn ");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ha presionado el botòn de interrogaciòn", "Mensaje con botòn Interrogaciòn");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ha presionado el botòn de Error", "Mensaje con botòn de Error");
+        }
+    }
+}
