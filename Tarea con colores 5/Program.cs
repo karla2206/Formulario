13b51fb0bbaa41c4780d3951bd98ccfde15d2c74
KARLA BEATRIZ CASTELLANOS SERPAS <USIS000621@ugb.edu.sv>
2021-08-30 15:27:23 -0600

    Cambio de menu strip

--- Tarea con colores 5/Program.cs
+++ Tarea con colores 5/Program.cs
@@ -16,7 +16,7 @@ namespace Tarea_con_colores_5
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menú());
         }
     }
 }
