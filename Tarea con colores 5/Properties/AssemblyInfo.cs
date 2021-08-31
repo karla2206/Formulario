835841529ce9855271c515c802536e9e0d8d8ebc
KARLA BEATRIZ CASTELLANOS SERPAS <USIS000621@ugb.edu.sv>
2021-08-30 21:31:05 -0600

    acerca de

--- Tarea con colores 5/Properties/AssemblyInfo.cs
+++ Tarea con colores 5/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Tarea con colores 5")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tecnology Center")]
+[assembly: AssemblyDescription("Venta en articulos electrónicos")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tarea con colores 5")]
+[assembly: AssemblyCompany("Tecnology Center S.A de C.V")]
+[assembly: AssemblyProduct("Todo en tecnologia")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Tecnology Center S.A de C.V")]
 [assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("es-SV")]
