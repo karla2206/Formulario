d5a83b08c079149910e1d9b1fef8e335e69b9cf2
JUAN ALFREDO MARTINEZ MOLINA <USIS001821@ugb.edu.sv>
2021-08-30 19:34:07 -0600

    login

---
+++ Tarea con colores 5/fLogin.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Tarea_con_colores_5
+{
+    partial class fLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtcontra = new System.Windows.Forms.TextBox();
+            this.baceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese su nombre de usuario";
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(124, 218);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(100, 20);
+            this.txtusuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese su contraseña";
+            // 
+            // txtcontra
+            // 
+            this.txtcontra.Location = new System.Drawing.Point(124, 295);
+            this.txtcontra.Name = "txtcontra";
+            this.txtcontra.Size = new System.Drawing.Size(100, 20);
+            this.txtcontra.TabIndex = 3;
+            // 
+            // baceptar
+            // 
+            this.baceptar.Location = new System.Drawing.Point(278, 313);
+            this.baceptar.Name = "baceptar";
+            this.baceptar.Size = new System.Drawing.Size(75, 23);
+            this.baceptar.TabIndex = 4;
+            this.baceptar.Text = "Aceptar";
+            this.baceptar.UseVisualStyleBackColor = true;
+            this.baceptar.Click += new System.EventHandler(this.baceptar_Click);
+            // 
+            // fLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.baceptar);
+            this.Controls.Add(this.txtcontra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.label1);
+            this.Name = "fLogin";
+            this.Text = "fLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtcontra;
+        private System.Windows.Forms.Button baceptar;
+    }
+}
